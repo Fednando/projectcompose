a05a3daeab2c3c0eea1688d8cdedfeb76ab4778e
Fernando Gomes Dos Santos <nando_santos2005@hotmail.com>
2025-04-09 19:08:58 -0300

    Calculator done

--- app/src/main/java/br/com/calorietracker/MainActivity.kt
+++ app/src/main/java/br/com/calorietracker/MainActivity.kt
@@ -1,11 +1,9 @@
 package br.com.calorietracker
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
